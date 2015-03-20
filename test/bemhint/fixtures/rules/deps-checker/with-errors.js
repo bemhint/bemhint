260f95fba9516591ee7d360872528de706ad049b
Evgeniy Gavryushin <job.egavr@yandex.ru>
2015-03-20 19:24:37 +0300

    Refactor the tests

--- test/bemhint/fixtures/rules/deps-checker/with-errors.js
+++ test/bemhint/fixtures/rules/deps-checker/with-errors.js
@@ -1,44 +1,44 @@
 module.exports = {
-    'blocks/block1': [
-        {
+    'blocks/block1': {
+        js: {
             name: 'block1.js',
             tech: 'js',
             path: 'blocks/block1/block1.js',
             content: 'BEM.DOM.decl()'
         },
-        {
+        bemhtml: {
             name: 'block1.bemhtml',
             tech: 'bemhtml',
             path: 'blocks/block1/block1.bemhtml',
             content: 'BEM.I18N()'
         },
-        {
+        'deps.js': {
             name: 'block1.deps.js',
             tech: 'deps.js',
             path: 'blocks/block1/block1.deps.js',
             content: '({ mustDeps: [] })'
         },
-        {
+        css: {
             name: 'block1.css',
             tech: 'css',
             path: 'blocks/block1/block1.css',
             content: 'css'
         }
-    ],
-    'blocks/block2': [
-        {
+    },
+    'blocks/block2': {
+        js: {
             name: 'block2.js',
             tech: 'js',
             path: 'blocks/block2/block2.js',
             content: 'BEM.decl()'
         }
-    ],
-    'blocks/block3': [
-        {
+    },
+    'blocks/block3': {
+        css: {
             name: 'block3.css',
             tech: 'css',
             path: 'blocks/block3/block3.css',
             content: 'css'
         }
-    ]
+    }
 };
---
+++ test/bemhint/rules/deps-checker.js
@@ -0,0 +1,234 @@
+var DepsChecker = require('../../../lib/rules/deps-checker'),
+    depsChecker = new DepsChecker();
+
+describe('deps-checker', function () {
+    describe('utils', function () {
+        describe('actual deps', function () {
+            it('must handle a case when deps are \'undefined\'', function () {
+                depsChecker._getActualDeps(undefined).must.be.eql({});
+            });
+
+            it('must handle a case when there are no pure \'mustDeps\'', function () {
+                depsChecker._getActualDeps({ content: '({ tech: "tech", mustDeps: "block" })' }).must.be.eql({});
+            });
+
+            it('must handle a case when there are several parts of pure \'mustDeps\'', function () {
+                // jscs:disable
+                var input = '([{ mustDeps: "i-bem" }, { mustDeps: {} }, { mustDeps: [{ block: "i-bem", elem: "dom" }] }])';
+
+                depsChecker._getActualDeps({ content: input }).must.be.eql('i-bem');
+            });
+
+            it('must get block \'i-bem\' from mustDeps given as String', function () {
+                depsChecker._getActualDeps({ content: '({ mustDeps: "i-bem" })' }).must.be.eql('i-bem');
+            });
+
+            it('must get block \'i-bem\' from mustDeps given as Object', function () {
+                depsChecker._getActualDeps({ content: '({ mustDeps: { block: "i-bem" } })' })
+                    .must.be.eql({ block: 'i-bem' });
+            });
+
+            it('must get block \'i-bem\' with elems from mustDeps given as Object', function () {
+                depsChecker._getActualDeps({ content: '({ mustDeps: { block: "i-bem", elems: ["dom", "html"] } })' })
+                    .must.be.eql({ block: 'i-bem', elems: ['dom', 'html'] });
+            });
+
+            it('must get block \'i-bem\' from mustDeps given as Object[]', function () {
+                var input = { content: '([{ mustDeps: [{ block: "i-bem" }] }])' },
+                    output = { block: 'i-bem' };
+
+                depsChecker._getActualDeps(input).must.be.eql(output);
+            });
+
+            it('must get block \'i-bem\' with elems from mustDeps given as Object[]', function () {
+                var input = { content: '([{ mustDeps: [{ block: "i-bem", elem: "dom" }] }])' },
+                    output = { block: 'i-bem', elem: 'dom' };
+
+                depsChecker._getActualDeps(input).must.be.eql(output);
+            });
+        });
+
+        describe('get expected deps', function () {
+            it('must NOT get deps', function () {
+                var input = {};
+
+                depsChecker._getExpectedDeps(input.js, input.bemhtml).must.be.eql({});
+            });
+
+            it('must get block \'i-bem\'', function () {
+                var input = { js: { content: 'js BEM.decl( js' } },
+                    output = { block: 'i-bem', elems: [] };
+
+                depsChecker._getExpectedDeps(input.js, input.bemhtml).must.be.eql(output);
+            });
+
+            it('must get block \'i-bem\' and elem \'html\' from JS content', function () {
+                var input = { js: { content: 'js BEM.HTML js' } },
+                    output = { block: 'i-bem', elems: ['html'] };
+
+                depsChecker._getExpectedDeps(input.js, input.bemhtml).must.be.eql(output);
+            });
+
+            it('must get block \'i-bem\' and elem \'html\' from BEMHTML content', function () {
+                var input = { bemhtml: { content: 'bemhtml' } },
+                    output = { block: 'i-bem', elems: ['html'] };
+
+                depsChecker._getExpectedDeps(input.js, input.bemhtml).must.be.eql(output);
+            });
+
+            it('must get block \'i-bem\' and elem \'dom\'', function () {
+                var input = { js: { content: 'js BEM.DOM.decl( js' } },
+                    output = { block: 'i-bem', elems: ['dom'] };
+
+                depsChecker._getExpectedDeps(input.js, input.bemhtml).must.be.eql(output);
+            });
+
+            it('must get block \'i-bem\' and elem \'internal\'', function () {
+                var input = { js: { content: 'js BEM.INTERNAL js' } },
+                    output = { block: 'i-bem', elems: ['internal'] };
+
+                depsChecker._getExpectedDeps(input.js, input.bemhtml).must.be.eql(output);
+            });
+
+            it('must get block \'i-bem\' and elem \'i18n\' from JS content', function () {
+                var input = { js: { content: 'js BEM.I18N( js' } },
+                    output = { block: 'i-bem', elems: ['i18n'] };
+
+                depsChecker._getExpectedDeps(input.js, input.bemhtml).must.be.eql(output);
+            });
+
+            it('must get block \'i-bem\' and elem \'i18n\' from BEMHTML content', function () {
+                var input = { bemhtml: { content: 'bemhtml BEM.I18N( bemhtml' } },
+                    output = { block: 'i-bem', elems: ['html', 'i18n'] };
+
+                depsChecker._getExpectedDeps(input.js, input.bemhtml).must.be.eql(output);
+            });
+
+            // jscs: disable
+            it('must get block \'i-bem\' and elem \'i18n\' from BEMHTML content (deprecated declaration of I18N)', function () {
+                var input = { bemhtml: { content: 'bemhtml \n    block  :\t \'i-bem\'  ,\n\n elem\t\n:  \'i18n\'' } },
+                    output = { block: 'i-bem', elems: ['html', 'i18n'] };
+
+                depsChecker._getExpectedDeps(input.js, input.bemhtml).must.be.eql(output);
+            });
+        });
+
+        describe('normalize deps', function () {
+            it('must normalize block \'i-bem\' given as String', function () {
+                depsChecker._normalizeDeps('i-bem').must.be.eql({ block: 'i-bem' });
+            });
+
+            it('must normalize block \'i-bem\' given as Object', function () {
+                depsChecker._normalizeDeps({ block: 'i-bem' }).must.be.eql({ block: 'i-bem', elems: [] });
+            });
+
+            it('must normalize block \'i-bem\' and elem given as String', function () {
+                var input = { block: 'i-bem', elem: 'dom' },
+                    output = { block: 'i-bem', elems: ['dom'] };
+
+                depsChecker._normalizeDeps(input).must.be.eql(output);
+            });
+
+            it('must normalize block \'i-bem\' and elem given as Object', function () {
+                var input = { block: 'i-bem', elem: { name: 'dom' } },
+                    output = { block: 'i-bem', elems: ['dom'] };
+
+                depsChecker._normalizeDeps(input).must.be.eql(output);
+            });
+
+            it('must normalize block \'i-bem\' and NOT get elem given as Object', function () {
+                var input = { block: 'i-bem', elem: { val: 'blah' } },
+                    output = { block: 'i-bem', elems: [] };
+
+                depsChecker._normalizeDeps(input).must.be.eql(output);
+            });
+
+            it('must normalize block \'i-bem\' and NOT get elem given as Object[]', function () {
+                var input = { block: 'i-bem', elem: ['dom', 'html'] },
+                    output = { block: 'i-bem', elems: [] };
+
+                depsChecker._normalizeDeps(input).must.be.eql(output);
+            });
+
+            it('must normalize block \'i-bem\' and get elems given as Object[]', function () {
+                var input = { block: 'i-bem', elems: ['dom', 'html'] },
+                    output = { block: 'i-bem', elems: ['dom', 'html'] };
+
+                depsChecker._normalizeDeps(input).must.be.eql(output);
+            });
+
+            it('must normalize block \'i-bem\' and get elems given as String', function () {
+                var input = { block: 'i-bem', elems: 'dom' },
+                    output = { block: 'i-bem', elems: ['dom'] };
+
+                depsChecker._normalizeDeps(input).must.be.eql(output);
+            });
+
+            it('must normalize block \'i-bem\' and get elems given as Object[]', function () {
+                var input = { block: 'i-bem', elems: [{ name: 'dom' }, { name: 'html' }] },
+                    output = { block: 'i-bem', elems: ['dom', 'html'] };
+
+                depsChecker._normalizeDeps(input).must.be.eql(output);
+            });
+
+            it('must normalize block \'i-bem\' and NOT get elems given as Object[]', function () {
+                var input = { block: 'i-bem', elems: [{ val: 'blah' }, ['blah']] },
+                    output = { block: 'i-bem', elems: [] };
+
+                depsChecker._normalizeDeps(input).must.be.eql(output);
+            });
+        });
+
+        describe('compare deps', function () {
+            it('must NOT be equal by \'block\' property', function () {
+                var input = { actualDeps: { block: 'i-bem' }, expectedDeps: {} };
+
+                depsChecker._isEqual(input.actualDeps, input.expectedDeps).must.be.false();
+            });
+
+            it('must NOT be equal by \'elems\' property', function () {
+                var input = {
+                    actualDeps: { block: 'i-bem', elems: ['dom', 'html'] },
+                    expectedDeps: { block: 'i-bem', elems: ['dom', 'html', 'i18n'] }
+                };
+
+                depsChecker._isEqual(input.actualDeps, input.expectedDeps).must.be.false();
+            });
+
+            it('must be equal', function () {
+                var input = {
+                    actualDeps: { block: 'i-bem', elems: ['internal', 'i18n', 'html', 'dom'] },
+                    expectedDeps: { block: 'i-bem', elems: ['dom', 'html', 'i18n', 'internal'] }
+                };
+
+                depsChecker._isEqual(input.actualDeps, input.expectedDeps).must.be.true();
+            });
+        });
+    });
+
+    describe('checker', function () {
+        it('must find deps errors', function () {
+            var entities = require('../fixtures/rules/deps-checker/with-errors'),
+                output = [
+                    {
+                        path: 'blocks/block1/block1.deps.js',
+                        actual: {},
+                        expected: { block: 'i-bem', elems: ['dom', 'html', 'i18n'] }
+                    },
+                    {
+                        path: 'blocks/block2',
+                        actual: 'No deps file block2.deps.js',
+                        expected: { block: 'i-bem' }
+                    }
+                ];
+
+            depsChecker.check(entities).must.be.eql(output);
+        });
+
+        it('must not find deps errors', function () {
+            var entities = require('../fixtures/rules/deps-checker/no-errors');
+
+            depsChecker.check(entities).must.be.eql([]);
+        });
+    });
+});
