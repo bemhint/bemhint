7d51835cea314c036feef96da8cdac9ac46fef04
egavr <job.egavr@yandex.ru>
2016-01-05 02:25:55 +0300

    add an html reporter

--- test/reporters/html.js
+++
@@ -1,9 +0,0 @@
-var htmlReporter = require('../../lib/reporters/html');
-
-describe('html reporter', function() {
-    it('should throw on attempt to use html reporter', function() {
-        (function() {
-            htmlReporter.write();
-        }).should.throw();
-    });
-});
