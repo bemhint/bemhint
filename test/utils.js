fffe49108b02c79f29b2aa6e26ba98b4892ef501
Vitaly Kosenko <vttly@yandex-team.ru>
2017-02-26 20:36:36 +0500

    rewrite tests with mock-fs

--- test/utils.js
+++ test/utils.js
@@ -1,7 +1,6 @@
 'use strict';
 
-var fs = require('fs'),
-    sinon = require('sinon'),
+var mockFs = require('mock-fs'),
     utils = require('../lib/utils');
 
 describe('utils', function() {
@@ -33,58 +32,19 @@ describe('utils', function() {
     });
 
     describe('.listTree', function() {
-        const dirStats = {isDirectory: () => true};
-        const fileStats = {isDirectory: () => false};
-
-        /**
-         * - foo
-         *   - foo/bar
-         *     - foo/bar/abc
-         *   - foo/baz
-         * - xyz
-         */
-
         beforeEach(() => {
-            sinon.stub(fs, 'stat', (path, callback) => {
-                switch(path) {
-                    case 'foo':
-                    case 'foo/bar':
-                        callback(null, dirStats);
-                        break;
-
-                    case 'foo/baz':
-                    case 'foo/bar/abc':
-                        callback(null, fileStats);
-                        break;
-
-                    case 'xyz':
-                        callback('stat error');
-                        break;
-                }
-            });
-
-            sinon.stub(fs, 'readdir', (path, callback) => {
-                switch(path) {
-                    case 'foo':
-                        callback(null, ['bar', 'baz']);
-                        break;
-
-                    case 'foo/bar':
-                        callback(null, ['abc']);
-                        break;
-
-                    case 'foo/baz':
-                    case 'foo/bar/abc':
-                    case 'xyz':
-                        callback('readdir error');
-                        break;
+            mockFs({
+                foo: {
+                    bar: {
+                        abc: 'abc-content'
+                    },
+                    baz: 'baz-content'
                 }
             });
         });
 
         afterEach(() => {
-            fs.stat.restore();
-            fs.readdir.restore();
+            mockFs.restore();
         });
 
         it('should guard with true/false', () =>
@@ -139,43 +99,15 @@ describe('utils', function() {
     });
 
     describe('.readIfFile', function() {
-        const dirStats = {isDirectory: () => true};
-        const fileStats = {isDirectory: () => false};
-
         beforeEach(() => {
-            sinon.stub(fs, 'stat', (path, callback) => {
-                switch(path) {
-                    case 'foo':
-                        callback(null, dirStats);
-                        break;
-
-                    case 'bar':
-                        callback(null, fileStats);
-                        break;
-
-                    case 'xyz':
-                        callback('stat error');
-                        break;
-                }
-            });
-
-            sinon.stub(fs, 'readFile', (path, callback) => {
-                switch(path) {
-                    case 'bar':
-                        callback(null, 'abc');
-                        break;
-
-                    case 'foo':
-                    case 'xyz':
-                        callback('readFile error');
-                        break;
-                }
+            mockFs({
+                foo: [],
+                bar: 'abc'
             });
         });
 
         afterEach(() => {
-            fs.stat.restore();
-            fs.readFile.restore();
+            mockFs.restore();
         });
 
         it('should return undefined for directory', () =>
